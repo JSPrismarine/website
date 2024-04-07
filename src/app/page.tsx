37d7f356564ad1c27f7a7f04ebbdbd84b8b04339
Filiph Siitam Sandström <filiph@nordcom.io>
2024-04-07 12:03:46 +0200

    Deps: Update nordstar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Heading, View } from '@nordcom/nordstar';
+import { Accented, Button, Card, Heading, Label, View } from '@nordcom/nordstar';
 import type { Metadata } from 'next';
 import styles from '@/app/page.module.scss';
 import Link from 'next/link';
@@ -16,36 +16,42 @@ export const metadata: Metadata = {
 export default async function IndexPage() {
     return (
         <View className={styles.container}>
-            <Card borderless={true} padding={false}>
-                <Heading level="h2" as="h1">
-                    a TypeScript Minecraft: Bedrock Edition Server
-                </Heading>
-            </Card>
+            <header className={`${styles.block} ${styles.header}`}>
+                <Card borderless={true} padding={false}>
+                    <Heading level="h1" className={styles.heading}>
+                        a <Accented>TypeScript</Accented> Minecraft: Bedrock Edition Server
+                    </Heading>
+                </Card>
+            </header>
 
-            <div className={styles.downloads}>
+            <div className={`${styles.block} ${styles.downloads}`}>
                 <Card className={styles.version}>
-                    <Heading level="h2">Nightly</Heading>
-                    <Heading level="h3">Highly unstable builds</Heading>
+                    <Label>vX.Y.Z</Label>
+                    <Heading level="h3" as="h2">
+                        Nightly builds
+                    </Heading>
                     <Card.Divider />
                     <Button variant="outline" as={Link} href="https://github.com/JSPrismarine/JSPrismarine">
-                        v0.0.0
+                        Nightly
                     </Button>
                 </Card>
+
                 <Card className={styles.version}>
-                    <Heading level="h2">Beta</Heading>
-                    <Heading level="h3">In-between builds</Heading>
+                    <Label>vX.Y.Z</Label>
+                    <Heading level="h3" as="h2">
+                        Testing builds
+                    </Heading>
                     <Card.Divider />
-                    <Button variant="outline" disabled>
-                        v0.0.0
-                    </Button>
+                    <Button variant="outline">Beta</Button>
                 </Card>
-                <Card className={styles.version}>
-                    <Heading level="h2">Release</Heading>
-                    <Heading level="h3">Properly tested build</Heading>
+
+                <Card color="primary" className={styles.version}>
+                    <Label>vX.Y.Z</Label>
+                    <Heading level="h3" as="h2">
+                        Release builds
+                    </Heading>
                     <Card.Divider />
-                    <Button color="secondary" variant="solid" disabled>
-                        v0.0.0
-                    </Button>
+                    <Button>Release</Button>
                 </Card>
             </div>
         </View>
